e1cc57f2ae2b6b0524e2e37d116fd92cd2818ea2
Arutjunov Levon <ltroop078@gmail.com>
2021-02-12 18:09:14 -0800

    API tells if slot is paid

--- city_api/main.py
+++ city_api/main.py
@@ -14,30 +14,57 @@ db = SQLAlchemy(app)
 # Create model and columns for sqlalchemy DB
 class ParkingModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    total_lots = db.Column(db.Integer, nullable=False)
-    free_lots = db.Column(db.Integer, nullable=True)
+    is_paid = db.Column(db.Boolean, nullable=False)
+    parking_slot = db.Column(db.Integer, nullable = True)
+
 
 # Initialize DB
 # NOTE: Run only once so you won't initialize DB every time you run script!!!!!!!!!!!!!!!
-# db.create_all()
+#db.create_all()
 
 # Arg parser for PUT
 parking_put_args = reqparse.RequestParser()
-parking_put_args.add_argument("total_lots", type=int, help="Total number of lots in parking space", required = True)
-parking_put_args.add_argument("free_lots", type=int, help="Number of available lots in parking space", required = True)
+parking_put_args.add_argument("is_paid", type=bool, help="Was parking lot paid for?", required = True)
+parking_put_args.add_argument("parking_slot", type=int, help="Number of parking slot", required = True)
 
 # Arg parser for UPDATE(patch)
 parking_update_args = reqparse.RequestParser()
-parking_update_args.add_argument("total_lots", type=int, help="Total number of lots in parking space")
-parking_update_args.add_argument("free_lots", type=int, help="Number of available lots in parking space")
+parking_update_args.add_argument("is_paid", type=bool, help="Was parking lot paid for?")
+parking_update_args.add_argument("parking_slot", type=int, help="Number of parking slot")
+
 
 
 
 resource_filed = {
     'id': fields.Integer,
-    'total_lots': fields.Integer,
-    'free_lots': fields.Integer
+    'parking_slot': fields.Integer,
+    'is_paid' : fields.Boolean
 }
+class ParkingSlot(Resource):
+    @marshal_with(resource_filed)
+    def get(self,parking_id,parking_slot):
+        """
+            BASIC GET REQUEST :
+            BASE = 'http://127.0.0.1:5000/'
+            response = requests.get(BASE + "parking/1/slot/1")
+            print(response.json())
+        """
+
+        # Find specified parking
+        parking_space = ParkingModel.query.filter_by(id=parking_id).first()
+
+        # Check if parking with specified id exists
+        if not parking_space:
+            abort(404, message="Could not find parking space with this id")
+
+        result = parking_space.query.filter_by(id=parking_id,parking_slot=parking_slot).first()
+
+        if not result:
+            abort(404, message="Could not find parking slot with this id")
+
+        return result
+
+api.add_resource(ParkingSlot, "/parking/<int:parking_id>/slot/<int:parking_slot>")
 
 class Parking(Resource):
     """
@@ -77,7 +104,7 @@ class Parking(Resource):
             abort(409, message = 'Parking id already exists')
 
         # Create new parking instance
-        parking = ParkingModel(id=parking_id, total_lots=args['total_lots'], free_lots=args['free_lots'])
+        parking = ParkingModel(id=parking_id, parking_slot=args['parking_slot'], is_paid=args['is_paid'])
 
         # Add new instance to DB
         db.session.add(parking)
@@ -97,10 +124,10 @@ class Parking(Resource):
         if not result:
             abort(404, message="Parking spot does not exist, cannot update")
 
-        if args['total_lots']:
-            ParkingModel.total_lots = args['total_lots']
-        if args['free_lots']:
-            ParkingModel.free_lots = args['free_lots']
+        if args['is_paid']:
+            ParkingModel.total_lots = args['is_paid']
+        if args['parking_slot']:
+            ParkingModel.free_lots = args['parking_slot']
 
         db.session.commit()
 
@@ -117,4 +144,4 @@ if __name__ == '__main__':
     """
     DO NOT USE "debug = True" on production deployment
     """
-    app.run(debug=True)
+    app.run(debug=False)
--- city_api/test.py
+++ city_api/test.py
@@ -2,16 +2,21 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'
 
-data = [{"total_lots":100,"free_lots":23},
-        {"total_lots":75,"free_lots":45},
-        {"total_lots":50,"free_lots":29},
+data = [{"is_paid": True, "parking_slot": 1},
+        {"is_paid": True, "parking_slot": 2},
+        {"is_paid": False, "parking_slot": 3},
         ]
 # Put some data
-for i in range(len(data)):
-    response = requests.put(BASE + "parking/" + str(i), data[i])
-    print(response.json())
+#for i in range(len(data)):
+#   response = requests.put(BASE + "parking/" + str(i) , data[i])
+#   print(response.json())
 
-# GET parking space with id - 1
-response = requests.get(BASE + "parking/1")
-print(response.json())
+# GET all parking info
+#for i in range(0,3):
+#     response = requests.get(BASE + 'parking/' + str(i))
+#     print(response.json())
 
+# GET info about specific parking slot
+# parking_id = i -> slot_id = i+1 ON THIS VERSION ONLY I HOPE
+#response = requests.get(BASE + 'parking/1/slot/2')
+#print(response.json())
