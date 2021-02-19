b013cc59163afba8854aa99532aa26cfd578b694
BlakkBerry <blakkberry2015@gmail.com>
2021-02-19 16:06:01 +0200

    Optimized imports

--- core/common/models.py
+++ core/common/models.py
@@ -1,5 +1,7 @@
-from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.db import models
+
+
 # Create your models here.
 
 class VideoProcessorType(models.Model):
--- core/common/views.py
+++ core/common/views.py
@@ -1,11 +1,10 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
 
-from django.shortcuts import get_object_or_404
-
-from .serializers import ParkingLotSerializer
 from .models import ParkingLot
+from .serializers import ParkingLotSerializer
 
 
 class ParkingList(APIView):
