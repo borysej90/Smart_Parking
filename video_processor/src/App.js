6d59404fa8cfa689c99353dfed945f21eabf4916
nebag9 <roma.zinko123@gmail.com>
2021-02-19 21:43:51 +0200

    Added ajax requests.

--- video_processor/src/App.js
+++ video_processor/src/App.js
@@ -4,74 +4,39 @@ import Lot from "./components/lot/Lot";
 
 
 function App() {
-    const [lots, setLots] = useState([
-        {
-            id: 1,
-            parking_side_id: 1,
-            is_occupied: false
-        },
-        {
-            id: 2,
-            parking_side_id: 2,
-            is_occupied: false
-        },
-        {
-            id: 3,
-            parking_side_id: 3,
-            is_occupied: false
-        },
-        {
-            id: 4,
-            parking_side_id: 4,
-            is_occupied: true
-        },
-        {
-            id: 5,
-            parking_side_id: 5,
-            is_occupied: false
-        },
-        {
-            id: 6,
-            parking_side_id: 6,
-            is_occupied: false
-        },
-        {
-            id: 7,
-            parking_side_id: 7,
-            is_occupied: true
-        },
-        {
-            id: 8,
-            parking_side_id: 8,
-            is_occupied: false
-        },
-        {
-            id: 9,
-            parking_side_id: 9,
-            is_occupied: false
-        },
-        {
-            id: 10,
-            parking_side_id: 10,
-            is_occupied: false
-        },
-        {
-            id: 11,
-            parking_side_id: 11,
-            is_occupied: false
-        },
-        {
-            id: 12,
-            parking_side_id: 12,
-            is_occupied: false
+    const [lots, setLots] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        const requestOptions = {
+            method: "GET",
+            headers: {"Content-Type": "application/json"},
         }
-    ])
+        fetch("http://localhost:8000/api/sites/1/lots/", requestOptions)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setLots(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
 
-    return (
-        <div className="App">
-            {lots.map(el => <Lot key={el.id} lot={el}/>)}
-        </div>
-    );
+    if (error) {
+        return <div>[ERROR]: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div className="App">
+                {lots.map(el => <Lot key={el.id} lot={el}/>)}
+            </div>
+        );
+    }
 }
 
 export default App;
--- video_processor/src/components/lot/Lot.js
+++ video_processor/src/components/lot/Lot.js
@@ -6,27 +6,39 @@ function Lot(props) {
     const [lot, setLot] = useState(props.lot)
 
 
-
-    function occupy(id) {
-
-        setLot({...lot, is_occupied: true})
+    function occupy() {
+        const requestOptions = {
+            method: "PUT",
+            headers: {'Content-Type': 'application/json;charset=UTF-8'},
+            body: JSON.stringify({...lot, is_occupied: !lot.is_occupied}),
+        };
+        fetch(`http://localhost:8000/api/sites/1/lots/${lot.id}/`, requestOptions)
+            .then(response => {
+                const data = response.json();
+                if (!response.ok) {
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+                setLot({...lot, is_occupied: !lot.is_occupied})
+            })
+            .catch(error => {
+                console.error("There was an error!", error);
+            });
     }
 
     return (
         <div className="lot-container-s">
             {
-                lot.parking_side_id % 6 === 0 ?
+                lot.id % 6 === 0 ?
                     <div className="lot-container-s">
-                        {lot.parking_side_id !== 6 ? <div className="hr"/> : null}
-                        <button onClick={() => occupy(lot.id)} disabled={lot.is_occupied}
-                                className={lot.is_occupied ? "lot lot-occupied" : "lot"}
-                                key={lot.id}>{lot.parking_side_id}</button>
+                        {lot.id !== 6 ? <div className="hr"/> : null}
+                        <button onClick={() => occupy()} className={lot.is_occupied ? "lot lot-occupied" : "lot"}
+                                key={lot.id}>{lot.id}</button>
                     </div>
                     :
                     <div className="lot-container">
-                        <button onClick={() => occupy(lot.id)} disabled={lot.is_occupied}
-                                className={lot.is_occupied ? "lot lot-occupied" : "lot"}
-                                key={lot.id}>{lot.parking_side_id}</button>
+                        <button onClick={() => occupy()} className={lot.is_occupied ? "lot lot-occupied" : "lot"}
+                                key={lot.id}>{lot.id}</button>
                     </div>
             }
         </div>
