fb015587c27b2eb9a8762dfa56e8f0ef59e26c64
BlakkBerry <blakkberry2015@gmail.com>
2021-02-19 15:59:58 +0200

    Updated serializer

--- core/common/serializers.py
+++ core/common/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
-from .models import ParkingLot
+from .models import ParkingLot, ParkingSite
 
 
 class ParkingLotSerializer(serializers.ModelSerializer):
+    parking_site_id = serializers.PrimaryKeyRelatedField(source='parking_site', queryset=ParkingSite.objects.all())
+
     class Meta:
         model = ParkingLot
-        fields = '__all__'
+        fields = ['id', 'coordinates', 'is_occupied', 'parking_site_id']
