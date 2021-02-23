690371db1d17004bef8aa03f2db9f959acae17be
ltroo <ltroop078@gmail.com>
2021-02-22 22:31:51 -0800

    Chenged in test.py too

--- city_api/test.py
+++ city_api/test.py
@@ -2,10 +2,10 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'
 
-response = requests.get(BASE + 'parking/Prospect P')
+response = requests.get(BASE + 'parking/Prospect_P')
 print(response.json())
 
-response = requests.get(BASE + 'parking/Prospect Ratushnyaka/lot_number/2')
+response = requests.get(BASE + 'parking/Prospect_Ratushnyaka/lot_number/2')
 print(response.json())
 
 
