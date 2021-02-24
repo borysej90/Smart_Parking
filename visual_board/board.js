f583de1a4e8e7033673b95b54fb1f1e60d592bae
NikitaKhramov <mantecanikita@gmail.com>
2021-02-24 17:03:04 +0200

    VB version_2.0

---
+++ visual_board/board.js
@@ -0,0 +1,123 @@
+//test data
+data = [
+    {id: 1, x_pos: 200, y_pos: 200, for_disabled: false, is_ocupied: true},
+    {id: 2, x_pos: 200, y_pos: 240, for_disabled: false, is_ocupied: false},
+    {id: 3, x_pos: 200, y_pos: 280, for_disabled: false, is_ocupied: false},
+    //
+    {id: 4, x_pos: 265, y_pos: 200, for_disabled: false, is_ocupied: false},
+    {id: 5, x_pos: 265, y_pos: 240, for_disabled: false, is_ocupied: true},
+    {id: 6, x_pos: 265, y_pos: 280, for_disabled: false, is_ocupied: false},
+    {id: 7, x_pos: 265, y_pos: 320, for_disabled: true, is_ocupied: true},
+    ////
+    {id: 8, x_pos: 450, y_pos: 200, for_disabled: true, is_ocupied: true},
+    {id: 9, x_pos: 450, y_pos: 260, for_disabled: true, is_ocupied: false},
+    {id: 10, x_pos: 450, y_pos: 320, for_disabled: true, is_ocupied: false},
+    {id: 11, x_pos: 450, y_pos: 380, for_disabled: true, is_ocupied: false},
+    {id: 12, x_pos: 450, y_pos: 440, for_disabled: false, is_ocupied: false},
+    {id: 13, x_pos: 450, y_pos: 480, for_disabled: false, is_ocupied: true},
+    {id: 14, x_pos: 450, y_pos: 520, for_disabled: false, is_ocupied: true},
+    //
+    {id: 15, x_pos: 515, y_pos: 200, for_disabled: true, is_ocupied: false},
+    {id: 16, x_pos: 515, y_pos: 260, for_disabled: true, is_ocupied: true}  ,
+    {id: 17, x_pos: 515, y_pos: 320, for_disabled: true, is_ocupied: false},
+    {id: 18, x_pos: 515, y_pos: 380, for_disabled: true, is_ocupied: true},
+    {id: 19, x_pos: 515, y_pos: 440, for_disabled: false, is_ocupied: false},
+    {id: 20, x_pos: 515, y_pos: 480, for_disabled: false, is_ocupied: true},
+    {id: 21, x_pos: 515, y_pos: 520, for_disabled: false, is_ocupied: false},
+    ////
+    {id: 22, x_pos: 700, y_pos: 200, for_disabled: false, is_ocupied: false},
+    {id: 23, x_pos: 700, y_pos: 240, for_disabled: false, is_ocupied: true},
+    {id: 24, x_pos: 700, y_pos: 280, for_disabled: false, is_ocupied: true},
+    {id: 25, x_pos: 700, y_pos: 320, for_disabled: false, is_ocupied: true},
+    {id: 26, x_pos: 700, y_pos: 360, for_disabled: false, is_ocupied: false},
+    {id: 27, x_pos: 700, y_pos: 400, for_disabled: false, is_ocupied: false},
+    {id: 28, x_pos: 700, y_pos: 440, for_disabled: false, is_ocupied: false},
+    {id: 29, x_pos: 700, y_pos: 480, for_disabled: false, is_ocupied: false},
+    {id: 30, x_pos: 700, y_pos: 520, for_disabled: false, is_ocupied: true},
+    //
+    {id: 31, x_pos: 765, y_pos: 200, for_disabled: false, is_ocupied: false},
+    {id: 32, x_pos: 765, y_pos: 240, for_disabled: false, is_ocupied: true},
+    {id: 33, x_pos: 765, y_pos: 280, for_disabled: false, is_ocupied: false},
+    {id: 34, x_pos: 765, y_pos: 320, for_disabled: false, is_ocupied: false},
+    {id: 35, x_pos: 765, y_pos: 360, for_disabled: false, is_ocupied: true},
+    {id: 36, x_pos: 765, y_pos: 400, for_disabled: false, is_ocupied: false},
+    {id: 37, x_pos: 765, y_pos: 440, for_disabled: false, is_ocupied: false},
+    {id: 38, x_pos: 765, y_pos: 480, for_disabled: false, is_ocupied: true},
+    {id: 39, x_pos: 765, y_pos: 520, for_disabled: false, is_ocupied: false},
+    ////
+    {id: 40, x_pos: 950, y_pos: 200, for_disabled: false, is_ocupied: false},
+    {id: 41, x_pos: 950, y_pos: 240, for_disabled: false, is_ocupied: true},
+    {id: 42, x_pos: 950, y_pos: 280, for_disabled: false, is_ocupied: false},
+    {id: 43, x_pos: 950, y_pos: 320, for_disabled: false, is_ocupied: true},
+    {id: 44, x_pos: 950, y_pos: 360, for_disabled: false, is_ocupied: false},
+    {id: 45, x_pos: 950, y_pos: 400, for_disabled: false, is_ocupied: false},
+    {id: 46, x_pos: 950, y_pos: 440, for_disabled: false, is_ocupied: false},
+    {id: 47, x_pos: 950, y_pos: 480, for_disabled: false, is_ocupied: false},
+    {id: 48, x_pos: 950, y_pos: 520, for_disabled: false, is_ocupied: true},
+    //
+    {id: 49, x_pos: 1015, y_pos: 200, for_disabled: false, is_ocupied: false},
+    {id: 50, x_pos: 1015, y_pos: 240, for_disabled: false, is_ocupied: true},
+    {id: 51, x_pos: 1015, y_pos: 280, for_disabled: false, is_ocupied: true},
+    {id: 52, x_pos: 1015, y_pos: 320, for_disabled: false, is_ocupied: true},
+    {id: 53, x_pos: 1015, y_pos: 360, for_disabled: false, is_ocupied: true},
+    {id: 54, x_pos: 1015, y_pos: 400, for_disabled: false, is_ocupied: true},
+    {id: 55, x_pos: 1015, y_pos: 440, for_disabled: false, is_ocupied: false},
+    {id: 56, x_pos: 1015, y_pos: 480, for_disabled: false, is_ocupied: true},
+    {id: 57, x_pos: 1015, y_pos: 520, for_disabled: false, is_ocupied: false}, 
+]
+
+//create color variables 
+red = "#ff6666";
+green = "#77e196";
+
+//create array of divs/slots
+slots = [];
+slots.length = data.length;
+
+//set slots on their places
+window.onload = function() {
+    for (var i = 0; i < data.length; i++) {
+
+        //create div/slot
+        slots[i] = document.createElement("div");
+        
+        //append div/slot to body
+        document.body.appendChild(slots[i]);
+        
+        //set slot id
+        slots[i].setAttribute("id", data[i].id);
+        
+        //place div/slot on possition
+        slots[i].style.position = "absolute";
+        slots[i].style.left = data[i].x_pos + 'px';
+        slots[i].style.top = data[i].y_pos + 'px';
+
+        //check if slot for disabled persons 
+        if (data[i].for_disabled == false) {
+            slots[i].className="slot";
+        }
+        else{
+            //img for slots for_disabled persons
+            img = document.createElement("img"); 
+            img.src = "images/logo_black.png";
+            img.className = "img_prop";
+
+            slots[i].className="slot_for_disabled";
+            slots[i].appendChild(img);
+        }
+    }
+}
+
+function ChangeColor(x) {
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].is_ocupied == true) {
+            slots[i].style.backgroundColor = red;
+        }
+        else{
+            slots[i].style.backgroundColor = green;
+        }
+    }
+}   
+
+//call ChangeColor function to see if something changed
+setInterval(ChangeColor, 1000);
