a2cc4b5f08a3b0e9ae08dc37610ccedc284ded61
borysej90 <borysej90@gmail.com>
2021-02-27 18:54:03 +0200

    Implemented Publisher that allows to send data to services

---
+++ core/events/publisher.py
@@ -0,0 +1,21 @@
+import environs
+import zmq
+
+from ..common import Singleton
+
+
+class Publisher(metaclass=Singleton):
+    """
+    Publisher is singleton which sends updates to all services.
+
+    Only sending Parking lots currently implemented.
+    """
+    def __init__(self):
+        port = environs.Env().str('ZMQ_PUB_PORT')
+        self._ctx = zmq.Context()
+        self._socket = self._ctx.socket(zmq.PUB)
+        self._socket.bind(f'tcp://*:{port}')
+
+    def send_parking_lots(self, parking_site_id, parking_lots):
+        self._socket.send(f'parking-{parking_site_id}_'.encode('utf-8'), flags=zmq.SNDMORE)
+        self._socket.send_pyobj(parking_lots)
