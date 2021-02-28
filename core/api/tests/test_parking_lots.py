a49bd1d0aa5bfbbbab7749d05e95b1731a11884f
BlakkBerry <blakkberry2015@gmail.com>
2021-02-28 19:49:28 +0200

    Created detail unit tests for parking-lots endpoint

--- core/api/tests/test_parking_lots.py
+++ core/api/tests/test_parking_lots.py
@@ -86,3 +86,99 @@ class ParkingLotsTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND,
                          'Status code is not 404. Given lot was found on improper parking site.')
+
+    def test_detail_put(self):
+        site = ParkingSite.objects.create(address='parking-site', lots_number=10, cameras_number=1, is_free=True)
+        lot = ParkingLot.objects.create(coordinates=[1, 1], parking_site=site, is_occupied=False)
+        new_lot = {'coordinates': [2, 2], 'is_occupied': True, 'parking_site_id': site.id}
+
+        url = reverse('parking-lot', args=[site.id, lot.id])
+        response = self.client.put(url, new_lot)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK, 'Status code is not 200.')
+        self.assertEqual(response.data['coordinates'], new_lot['coordinates'],
+                         'Updated "coordinates" field is not equal to the requested one.')
+        self.assertEqual(response.data['is_occupied'], new_lot['is_occupied'],
+                         'Updated "is_occupied" field is not equal to the requested ones.')
+        self.assertEqual(response.data['parking_site_id'], new_lot['parking_site_id'],
+                         'Updated "parking_site_id" field, is not equal to the requested ones.')
+
+    def test_detail_put_with_another_parking_site(self):
+        site1 = ParkingSite.objects.create(address='parking-site-1', lots_number=10, cameras_number=1, is_free=True)
+        site2 = ParkingSite.objects.create(address='parking-site-2', lots_number=20, cameras_number=2, is_free=False)
+        lot = ParkingLot.objects.create(coordinates=[1, 1], parking_site=site1, is_occupied=False)
+        new_lot = {'coordinates': [2, 2], 'is_occupied': True, 'parking_site_id': site1.id}
+
+        url = reverse('parking-lot', args=[site2.id, lot.id])
+        response = self.client.put(url, new_lot)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND,
+                         'Status code is not 404. Given lot was found on improper parking site.')
+
+    def test_put_with_invalid_data(self):
+        site = ParkingSite.objects.create(address='parking-site', lots_number=10, cameras_number=1, is_free=True)
+        lot = ParkingLot.objects.create(coordinates=[1, 1], parking_site=site, is_occupied=False)
+        new_lot = {'coordinates': 5, 'is_occupied': 'invalid boolean', 'parking_site_id': site.id}
+
+        url = reverse('parking-lot', args=[site.id, lot.id])
+        response = self.client.put(url, new_lot)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST,
+                         'Status code is not 400.')
+
+    def test_detail_patch(self):
+        site = ParkingSite.objects.create(address='parking-site', lots_number=10, cameras_number=1, is_free=True)
+        lot = ParkingLot.objects.create(coordinates=[1, 1], parking_site=site, is_occupied=False)
+        new_lot = {'coordinates': [2, 2]}
+
+        url = reverse('parking-lot', args=[site.id, lot.id])
+        response = self.client.patch(url, new_lot)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK, 'Status code is not 200.')
+        self.assertEqual(response.data['coordinates'], new_lot['coordinates'],
+                         'Patched "coordinates" field is not equal to the requested one.')
+
+    def test_detail_patch_with_another_parking_site(self):
+        site1 = ParkingSite.objects.create(address='parking-site-1', lots_number=10, cameras_number=1, is_free=True)
+        site2 = ParkingSite.objects.create(address='parking-site-2', lots_number=20, cameras_number=2, is_free=False)
+        lot = ParkingLot.objects.create(coordinates=[1, 1], parking_site=site1, is_occupied=False)
+        new_lot = {'coordinates': [2, 2]}
+
+        url = reverse('parking-lot', args=[site2.id, lot.id])
+        response = self.client.patch(url, new_lot)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND,
+                         'Status code is not 404. Given lot was found on improper parking site.')
+
+    def test_patch_with_invalid_data(self):
+        site = ParkingSite.objects.create(address='parking-site', lots_number=10, cameras_number=1, is_free=True)
+        lot = ParkingLot.objects.create(coordinates=[1, 1], parking_site=site, is_occupied=False)
+        new_lot = {'coordinates': True, 'something': [1, 2, 3]}
+
+        url = reverse('parking-lot', args=[site.id, lot.id])
+        response = self.client.patch(url, new_lot)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST,
+                         'Status code is not 400.')
+
+    def test_detail_delete(self):
+        site = ParkingSite.objects.create(address='parking-site', lots_number=10, cameras_number=1, is_free=True)
+        lot = ParkingLot.objects.create(coordinates=[1, 1], parking_site=site, is_occupied=False)
+
+        url = reverse('parking-lot', args=[site.id, lot.id])
+        response = self.client.delete(url)
+        deleted_lot = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK, 'Status code is not 200.')
+        self.assertEqual(deleted_lot.status_code, status.HTTP_404_NOT_FOUND, 'Parking lot wasn\'t deleted.')
+
+    def test_detail_delete_with_another_parking_site(self):
+        site1 = ParkingSite.objects.create(address='parking-site-1', lots_number=10, cameras_number=1, is_free=True)
+        site2 = ParkingSite.objects.create(address='parking-site-2', lots_number=20, cameras_number=2, is_free=False)
+        lot = ParkingLot.objects.create(coordinates=[1, 1], parking_site=site1, is_occupied=False)
+
+        url = reverse('parking-lot', args=[site2.id, lot.id])
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND,
+                         'Status code is not 404. Given lot was found on improper parking site.')
