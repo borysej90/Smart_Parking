f218f4b54b6b329a7889d62c69d818ae8d1b311b
BlakkBerry <blakkberry2015@gmail.com>
2021-02-28 18:41:14 +0200

    Created GET unit tests for 'parking-lot' endpoint

--- core/api/tests/test_parking_lots.py
+++ core/api/tests/test_parking_lots.py
@@ -1,8 +1,8 @@
 import json
 
 from django.urls import reverse
-from rest_framework.test import APITestCase
 from rest_framework import status
+from rest_framework.test import APITestCase
 
 from api.models import ParkingLot, ParkingSite
 from api.serializers import ParkingLotSerializer
@@ -63,3 +63,26 @@ class ParkingLotsTestCase(APITestCase):
         response = self.client.post(url, json.dumps(lots), content_type='application/json')
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, 'Status code is not 400.')
+
+    def test_detail_get(self):
+        site = ParkingSite.objects.create(address='parking-site-1', lots_number=10, cameras_number=1, is_free=True)
+        lot = ParkingLot.objects.create(coordinates=[1, 1], parking_site=site, is_occupied=False)
+
+        url = reverse('parking-lot', args=[site.id, lot.id])
+        response = self.client.get(url)
+
+        serializer_data = ParkingLotSerializer(lot).data
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK, 'Status code is not 200.')
+        self.assertEqual(response.data, serializer_data, 'Expected object is not equal to the created one.')
+
+    def test_detail_get_with_another_parking_site(self):
+        site1 = ParkingSite.objects.create(address='parking-site-1', lots_number=10, cameras_number=1, is_free=True)
+        site2 = ParkingSite.objects.create(address='parking-site-2', lots_number=20, cameras_number=2, is_free=False)
+        lot = ParkingLot.objects.create(coordinates=[1, 1], parking_site=site1, is_occupied=False)
+
+        url = reverse('parking-lot', args=[site2.id, lot.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND,
+                         'Status code is not 404. Given lot was found on improper parking site.')
