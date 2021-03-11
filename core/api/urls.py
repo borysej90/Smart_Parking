b87fe02a4f64451b302aa3ee0e37efd9f19f8236
borysej90 <borysej90@gmail.com>
2021-03-11 22:33:54 +0200

    Reformatted urls in API

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
+processors_urls = [
+    path('map/', views.get_parking_lots_map, name='get-parking-lots-map'),
+    path('rtsp/', views.get_camera_stream_url, name='get-camera-stream-url'),
+]
+
+sites_urls = [
+    path('lots/', views.ParkingList.as_view(), name='parking-lots'),
+    path('lots/<int:pk>/', views.ParkingDetail.as_view(), name='parking-lot'),
+]
+
 urlpatterns = [
-    path('sites/<int:site_id>/lots/', views.ParkingList.as_view(), name='parking-lots'),
-    path('sites/<int:site_id>/lots/<int:pk>/', views.ParkingDetail.as_view(), name='parking-lot'),
-    path('processors/<int:processor_id>/map/', views.get_parking_lots_map, name='get-parking-lots-map'),
-    path('processors/<int:processor_id>/rtsp/', views.get_camera_stream_url, name='get-camera-stream-url')
+    path('sites/<int:site_id>/', include(sites_urls)),
+    path('processors/<int:processor_id>/', include(processors_urls)),
 ]
