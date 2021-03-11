45cde2667350e54e691f03beb7967da5ca2c1a03
NikitaKhramov <mantecanikita@gmail.com>
2021-03-11 20:44:03 +0200

    delete the code from admin.py

--- core/api/admin.py
+++ core/api/admin.py
@@ -1,8 +1,1 @@
 # from django.contrib import admin
-from django.contrib import admin
-from .models import VideoProcessor, VideoProcessorType, ParkingSite, ParkingLot
-
-admin.site.register(VideoProcessor)
-admin.site.register(ParkingSite)
-admin.site.register(ParkingLot)
-admin.site.register(VideoProcessorType)
