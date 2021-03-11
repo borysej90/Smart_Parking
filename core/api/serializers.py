0b53641bb0e7fc154fd4fc4c83f1d7d0044fecfc
BlakkBerry <blakkberry2015@gmail.com>
2021-03-11 19:12:04 +0200

    Updated serializers according to new cords format

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -3,8 +3,36 @@ from rest_framework import serializers
 from .models import ParkingLot, ParkingSite
 
 
+class PointSerializer(serializers.Serializer):
+    x = serializers.IntegerField()
+    y = serializers.IntegerField()
+
+
 class ParkingLotSerializer(serializers.ModelSerializer):
     parking_site_id = serializers.PrimaryKeyRelatedField(source='parking_site', queryset=ParkingSite.objects.all())
+    coordinates = PointSerializer(many=True)
+
+    def to_internal_value(self, data):
+        validated_data = super().to_internal_value(data)
+
+        coordinates = []
+        for points in validated_data['coordinates']:
+            coordinates.extend(list(points.values()))
+
+        validated_data['coordinates'] = coordinates
+
+        return validated_data
+
+    def to_representation(self, instance):
+        coordinates = []
+        for x, y in zip(instance.coordinates[0::2], instance.coordinates[1::2]):
+            coordinates.append({'x': x, 'y': y})
+
+        instance.coordinates = coordinates
+
+        validated_data = super().to_representation(instance)
+
+        return validated_data
 
     class Meta:
         model = ParkingLot
