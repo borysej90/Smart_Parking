a2092170a785d34d6589b27de9f744d23c99d02c
BlakkBerry <57462326+BlakkBerry@users.noreply.github.com>
2021-03-26 13:40:22 +0200

    Create endpoint for parking sites (#64)
    * Added serializer and view for Parking sites
    
    * Renamed parking site view
    
    * Formatted code

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -5,8 +5,16 @@ from .models import ParkingLot, ParkingSite, VideoProcessor
 
 class ParkingLotSerializer(serializers.ModelSerializer):
     parking_site_id = serializers.PrimaryKeyRelatedField(source='parking_site', queryset=ParkingSite.objects.all())
-    video_processor_id = serializers.PrimaryKeyRelatedField(source='video_processor', queryset=VideoProcessor.objects.all())
+    video_processor_id = serializers.PrimaryKeyRelatedField(source='video_processor',
+                                                            queryset=VideoProcessor.objects.all())
 
     class Meta:
         model = ParkingLot
-        fields = ['id', 'position_on_board', 'shape_on_board', 'coordinates', 'is_occupied', 'parking_site_id', 'video_processor_id', 'is_for_disabled']
+        fields = ['id', 'position_on_board', 'shape_on_board', 'coordinates', 'is_occupied', 'parking_site_id',
+                  'video_processor_id', 'is_for_disabled']
+
+
+class ParkingSiteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ParkingSite
+        fields = '__all__'
